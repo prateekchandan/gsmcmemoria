5683283a21f8138cbbea1b212902b62210402dca
Prateek Chandan <prateekchandan5545@gmail.com>
2015-11-29 03:27:34 +0530

    amount correct

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,7 +1,7 @@
 <b>Hi {{$name}}</b>
 <br>
 <p>
-	You have successfully completed the payment of <b> Rs {{$amount}}</b> for the passes. Please keep safe your payment Id : <b>{{$id}}</b> for any further communcations.	
+	You have successfully completed the payment of <b> Rs {{$amount/100.0}}</b> for the passes. Please keep safe your payment Id : <b>{{$id}}</b> for any further communcations.	
 </p>
 <p>
 	You shall receive your invitation card by the first week of January. 
